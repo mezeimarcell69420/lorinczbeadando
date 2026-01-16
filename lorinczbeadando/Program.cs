d63c620a692083f6759bf1afcb1809494f52ef72
mezeimarcell69420 <mezeimarcell2fa@gmail.com>
2026-01-16 08:33:02 +0100

    Smaller

--- lorinczbeadando/Program.cs
+++ lorinczbeadando/Program.cs
@@ -78,22 +78,6 @@
             /// <returns>A string response</returns>
             public string Season(int month)
             {
-                if (month >= 1 && month <= 3)
-                {
-                    return "winter";
-                }
-                else if (month >= 4 && month <= 6)
-                {
-                    return "spring";
-                }
-                else if (month >= 7 && month <= 9)
-                {
-                    return "summer";
-                }
-                else if (month >= 10 && month <= 12)
-                {
-                    return "fall";
-                }
 
                 // TODO : Return season based on month value
                 throw new NotImplementedException();
